be648642c86a66fa46927cc95366f274ee373675
Hamish Coleman <hamish@zot.org>
2017-06-28 20:06:52 +0800

    Move the board rules for creating a debian root into the common makefile

--- boards/common.mk
+++ boards/common.mk
@@ -2,11 +2,23 @@
 # functions and definitions expected to be used my many or all systems
 #
 
+# Calculate the basename of the debian build file
+DEBIAN_BASENAME = debian.$(DEBIAN_VER).$(DEBIAN_ARCH)
+DEBIAN = ../../debian/build/$(DEBIAN_BASENAME)
+
 # install any packages needed for the builder
 build-depends: $(TAG)/build-depends
 $(TAG)/build-depends: Makefile
 	sudo apt-get -y install $(BUILD_DEPENDS)
 	$(call tag,build-depends)
+
+# Rules to go and make the debian installed root
+# Note: this has no dependancy checking, and will simply use what ever
+# file is there
+$(DEBIAN).cpio:
+	$(MAKE) -C ../../debian build/$(DEBIAN_BASENAME).cpio CONFIG_DEBIAN_ARCH=$(DEBIAN_ARCH)
+$(DEBIAN).lzma:
+	$(MAKE) -C ../../debian build/$(DEBIAN_BASENAME).lzma CONFIG_DEBIAN_ARCH=$(DEBIAN_ARCH)
 
 %.lzma: %.cpio
 	lzma <$< >$@
