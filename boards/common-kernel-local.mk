ef98c12cd3c5b4bf455d6575ce090a1fbcbe073e
Hamish Coleman <hamish@zot.org>
2017-07-11 11:54:24 +0800

    Replace the orange pi zero kernel with our own build - one with working wifi

---
+++ boards/common-kernel-local.mk
@@ -0,0 +1,19 @@
+#
+# Rules to use the local kernel build dir
+#
+
+# FIXME
+# - add an arch to the zImage
+
+LOCAL_KERNEL = ../../linux/build/linux/zImage
+LOCAL_MODULES = ../../linux/build/modules-$(DEBIAN_ARCH).lzma
+
+$(LOCAL_KERNEL):
+	$(MAKE) -C ../../linux build/linux/zImage DEBIAN_ARCH=$(DEBIAN_ARCH)
+
+$(addsuffix .cpio,$(basename $(LOCAL_MODULES))):
+	$(MAKE) -C ../../linux build/linux/modules-$(DEBIAN_ARCH).cpio DEBIAN_ARCH=$(DEBIAN_ARCH)
+
+$(BUILD)/boot/dtb/%.dtb: ../../linux/build/linux/dtb/%.dtb
+	mkdir -p $(dir $@) 
+	cp $< $@
