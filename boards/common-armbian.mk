640f638fe20ba51b4bd6ab4ed852781d9e106ef5
Hamish Coleman <hamish@zot.org>
2017-07-13 08:51:27 +0800

    Download and check the repository keys in the deb downloader script.
    This adds a feature that was not easily possible with multistrap as
    the armbian and raspberrypi repos both did not seem to have package
    containing the key - just the assumption that users of the repo would
    have been built in their build system and magically have the key.

--- boards/common-armbian.mk
+++ boards/common-armbian.mk
@@ -2,6 +2,7 @@
 # The definitions needed to download packages from the armbian repo
 #
 
+ARMBIAN_REPOKEY = https://apt.armbian.com/armbian.key
 ARMBIAN_REPOURI = http://apt.armbian.com
 ARMBIAN_REPOSUITE = jessie
 
--- boards/common-raspbian.mk
+++ boards/common-raspbian.mk
@@ -2,5 +2,6 @@
 # The definitions needed to download packages from the raspbian repo
 #
 
+RASPBIAN_REPOKEY = https://archive.raspberrypi.org/debian/raspberrypi.gpg.key
 RASPBIAN_REPOURI = http://archive.raspberrypi.org/debian/
 RASPBIAN_REPOSUITE = jessie
