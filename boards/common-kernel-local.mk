19e5741bfc8b8305fc48321d69542e1a37a1f9ed
Hamish Coleman <hamish@zot.org>
2017-07-13 17:13:06 +0800

    Fix bug with the name of the kernel modules file

--- boards/common-kernel-local.mk
+++ boards/common-kernel-local.mk
@@ -12,7 +12,7 @@ $(LOCAL_KERNEL):
 	$(MAKE) -C ../../linux build/linux/zImage DEBIAN_ARCH=$(DEBIAN_ARCH)
 
 $(addsuffix .cpio,$(basename $(LOCAL_MODULES))):
-	$(MAKE) -C ../../linux build/linux/modules-$(DEBIAN_ARCH).cpio DEBIAN_ARCH=$(DEBIAN_ARCH)
+	$(MAKE) -C ../../linux build/modules-$(DEBIAN_ARCH).cpio DEBIAN_ARCH=$(DEBIAN_ARCH)
 
 $(BUILD)/boot/dtb/%.dtb: ../../linux/build/linux/dtb/%.dtb
 	mkdir -p $(dir $@) 
