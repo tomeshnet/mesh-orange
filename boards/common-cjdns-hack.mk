1ee49c7e23c1f56f1a9365d022f4193e59282a7a
Hamish Coleman <hamish@zot.org>
2018-01-04 08:20:04 +0800

    Something is wrong with the ban.ai website hosting the cjdns repo - disable it

--- boards/common-cjdns-hack.mk
+++ boards/common-cjdns-hack.mk
@@ -7,6 +7,11 @@ ifeq ($(DEBIAN_ARCH),armhf)
 ../../cjdns/hack/build/cjdns.cpio:
 	$(MAKE) -C ../../cjdns/hack DEBIAN_ARCH=$(DEBIAN_ARCH) build/cjdns.cpio
 
-INITRD_PARTS += ../../cjdns/hack/build/cjdns.lzma
+# Temporary disable:
+# Something is wrong with the ban.ai server - when using wget or curl,
+# it sends the wrong SSL certificate (it is sending the cert for the server
+# hosting the site: prone.ws)
+#
+#INITRD_PARTS += ../../cjdns/hack/build/cjdns.lzma
 
 endif
