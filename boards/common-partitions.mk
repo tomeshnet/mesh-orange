960fac1425af9352e4f371c10c6fc7fe0800f635
Hamish Coleman <hamish@zot.org>
2018-02-05 15:56:37 +1100

    Add missing parts to allow consolidating the mtoolsrc handling in the full-image builder used for the licheepi

--- boards/common-partitions.mk
+++ boards/common-partitions.mk
@@ -3,7 +3,25 @@
 # diskimages
 #
 
+# The automatically created partition layout is always the same:
+# - part1, vfat, containing boot files and initrd
+# - part2, optional swap partition
+# - part3, optional linux partition.  At end of disk to allow expanding to fill
+#       the whole sdcard.  Could contain the root filesystem on a non-ramdisk
+#       image, or just bulk persistant storage for something in the ramdisk
+#
+
 PART1_SIZE_MEGS ?= 1000
+#PART2_SIZE_MEGS ?= 1000 # reserved for swap
+#PART3_SIZE_MEGS ?= 1000 # ext2 root
+
+PART1_BEGIN_SEC = $$((0x100000/512))
+ifdef PART2_SIZE_MEGS
+PART2_BEGIN_SEC = $$(( $(PART1_BEGIN_SEC) + ($(PART1_SIZE_MEGS) *1024*1024) /512))
+ifdef PART3_SIZE_MEGS
+PART3_BEGIN_SEC = $$(( $(PART2_BEGIN_SEC) + ($(PART2_SIZE_MEGS) *1024*1024) /512))
+endif
+endif
 
 BUILD_DEPENDS += mtools
 
@@ -21,4 +39,11 @@ endef
 $(BUILD)/mtoolsrc: Makefile
 	mkdir -p $(dir $@)
 	echo 'drive z: file="$(DISK_IMAGE).tmp" cylinders=$(PART1_SIZE_MEGS) heads=64 sectors=32 partition=1 mformat_only' >$@
+	if [ -n "$(PART2_SIZE_MEGS)" ]; then \
+            echo 'drive y: file="$(DISK_IMAGE).tmp" cylinders=$(PART2_SIZE_MEGS) heads=64 sectors=32 partition=2 mformat_only' >>$@; \
+	fi
+	if [ -n "$(PART3_SIZE_MEGS)" ]; then \
+            echo 'drive x: file="$(DISK_IMAGE).tmp" cylinders=$(PART3_SIZE_MEGS) heads=64 sectors=32 partition=3 mformat_only' >>$@; \
+	fi
+
 CLEAN_FILES += $(BUILD)/mtoolsrc
