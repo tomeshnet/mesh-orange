dbaa780c4956f05c855d1559dc1dfadbf0ad8d8f
Hamish Coleman <hamish@zot.org>
2018-02-16 17:05:04 +0800

    All disk image steps now standarised

--- boards/common-allwinner.mk
+++ boards/common-allwinner.mk
@@ -8,8 +8,7 @@
 # $1 is the SPL
 # $2 is the output board file
 define allwinner_spl
-    truncate --size=$$((0x2000)) $2  # skip to correct offset for SPL
-    cat $1 >>$2                      # add the SPL+uboot binary
+    truncate --size=">$$((0x2000))" $2  # ensure large enough file for SPL
+    dd if=$1 of=$2 conv=notrunc bs=$$((0x2000)) seek=1
 endef
-
 
--- boards/common-uboot.mk
+++ boards/common-uboot.mk
@@ -15,12 +15,44 @@ endif
 # $1 is the mtools config file
 # $2 is the mtools drive letter
 # $3 is the list of files for the /boot dir
-# $4 is the list of files for the /boot/dtb dir
-define uboot_copyfiles
+define uboot_copy_bootfiles
     MTOOLSRC=$1 mmd $2boot
-    MTOOLSRC=$1 mmd $2boot/dtb
     MTOOLSRC=$1 mcopy $3 $2boot
-    MTOOLSRC=$1 mcopy $4 $2boot/dtb
+endef
+
+# Create boot/dtb directory and copy the lists of files
+#
+# $1 is the mtools config file
+# $2 is the mtools drive letter
+# $3 is the list of files for the /boot/dtb dir
+define uboot_copy_dtbfiles
+    MTOOLSRC=$1 mmd $2boot/dtb
+    MTOOLSRC=$1 mcopy $3 $2boot/dtb
+endef
+
+# Create boot/dtb/overlay directory and copy the lists of files
+#
+# $1 is the mtools config file
+# $2 is the mtools drive letter
+# $3 is the list of files for the /boot/dtb/overlay dir
+define uboot_copy_overlayfiles
+    MTOOLSRC=$1 mmd $2boot/dtb/overlay
+    MTOOLSRC=$1 mcopy $3 $2boot/dtb/overlay
+endef
+
+# Overwrite the internet uboot default environment
+# Usually expected to be used to work around strange environment
+# issues.
+#
+# TODO
+# - The image file is a key=value text file with defined separators
+#   and a CRC, so we should be creating the binary from a text input
+#   and not just jamming a blob into another blob
+#
+# $1 is the replacement env
+# $2 is the image file
+define uboot_overwrite_environment
+    dd if=$1 of=$2 conv=notrunc bs=$$((0x088000)) seek=1
 endef
 
 BUILD_DEPENDS += mtools
