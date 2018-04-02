430118bc84b7f733c22e30ca6e8b5f986c2efe4d
Hamish Coleman <hamish@zot.org>
2018-04-02 23:49:00 +0800

    Extract all repo definitions into one common place

---
+++ apt.sources/README.md
@@ -0,0 +1,2 @@
+This directory contains files to define the settings needed for each
+upstream dpkg repository.
--- mk/common-raspbian.mk
+++
@@ -1,7 +0,0 @@
-#
-# The definitions needed to download packages from the raspbian repo
-#
-
-RASPBIAN_KEY = https://archive.raspberrypi.org/debian/raspberrypi.gpg.key
-RASPBIAN_REPO = "deb http://archive.raspberrypi.org/debian/ jessie main"
-
